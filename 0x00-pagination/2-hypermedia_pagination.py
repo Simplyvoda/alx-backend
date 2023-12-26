421570502dac08db9dcf98930db6120bc05bcb78
Vodina  Efem <efem.vodina@gmail.com>
2023-12-26 21:48:19 +0100

    Update 2-hypermedia_pagination.py

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,1 +1,59 @@
+#!/usr/bin/env python3
+"""
+This module contains a function
+that returns the start and end index
+to return in a list
+"""
+import csv
+import math
+from typing import List, Tuple
 
+
+def index_range(page: int, page_size: int) -> Tuple[int]:
+    """
+    This function returns the start and end
+    index in a tuple
+    """
+    start_index = (page * page_size) - page_size
+    end_index = (page * page_size)
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Handles task 1 requirement
+        """
+        assert isinstance(page, int) and page > 0, \
+            "Page number should be a positive integer"
+        assert isinstance(page_size, int) and page_size > 0, \
+            "Page size should be a positive integer"
+
+        self.__page = page
+        self.__page_size = page_size
+
+        start_index, end_index = index_range(page, page_size)
+        dataset = self.dataset()
+
+        if start_index >= len(dataset):
+            return []
+
+        return dataset[start_index:end_index]
