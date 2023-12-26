32df23815497b782aeb870f7c308e6683d2a4836
Vodina Efem <efem.vodina@gmail.com>
2023-12-26 19:50:15 +0000

    Task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,1 +1,17 @@
+#!/usr/bin/env python3
+"""
+This module contains a function
+that returns the start and end index
+to return in a list
+"""
+from typing import Tuple
 
+
+def index_range(page: int, page_size: int) -> Tuple[int]:
+    """
+    This function returns the start and end
+    index in a tuple
+    """
+    start_index = (page * page_size) - page_size
+    end_index = (page * page_size)
+    return (start_index, end_index)
