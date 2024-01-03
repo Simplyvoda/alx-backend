1ac390486f20ab4d9e8c5e7b1dd6901a389bdee0
Simplyvoda <efem.vodina@gmail.com>
2024-01-03 07:54:55 +0000

    task 1 added

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+'''
+This module contains a function
+that inherits from BaseCaching
+'''
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    '''
+    Implementing First in First Out
+    Cache replacement policy
+    '''
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        '''
+        Assigns to the self.cache_data dict
+        the item value for the key
+        Dict[Key] = Value
+        '''
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                first_key = list(self.cache_data.keys())[0]
+                print(f"DISCARD: {first_key}\n")
+                del (self.cache_data[first_key])
+                return self.cache_data
+            return self.cache_data
+
+    def get(self, key):
+        '''
+        Returns value linked to key in
+        self.cache_data
+        '''
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
