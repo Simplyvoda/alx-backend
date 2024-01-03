9879a45b46b412e3fe3df8e261b0575030dbbdd7
Simplyvoda <efem.vodina@gmail.com>
2024-01-03 09:07:29 +0000

    Changed del to pop on Task 1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -6,44 +6,42 @@ that inherits from BaseCaching
 
 from collections import OrderedDict
 
-
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    '''
+  '''
     Implementing First in First Out
     Cache replacement policy
     '''
-    def __init__(self):
-        super().__init__()
-        self.cache_data = OrderedDict()
 
-    def put(self, key, item):
-        '''
+  def __init__(self):
+    super().__init__()
+    self.cache_data = OrderedDict()
+
+  def put(self, key, item):
+    '''
         Assigns to the self.cache_data dict
         the item value for the key
         Dict[Key] = Value
         Checks if the dict has exceeded max size
         and deletes using FIFO policy
         '''
-        if key is None or item is None:
-            return
+    if key is None or item is None:
+      return
 
-        self.cache_data[key] = item
+    self.cache_data[key] = item
 
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first_key = list(self.cache_data.keys())[0]
-            print(f"DISCARD: {first_key}\n")
-            del self.cache_data[first_key]
-            return self.cache_data
-        return self.cache_data
+    if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+      first_key = list(self.cache_data.keys())[0]
+      print(f"DISCARD: {first_key}\n")
+      self.cache_data.pop(first_key)
 
-    def get(self, key):
-        '''
+  def get(self, key):
+    '''
         Returns value linked to key in
         self.cache_data
         '''
-        if key is not None and key in self.cache_data:
-            return self.cache_data[key]
-        return None
+    if key is not None and key in self.cache_data:
+      return self.cache_data[key]
+    return None
