553bf687d866c4c28ac7675375a577bce929ca88
Simplyvoda <efem.vodina@gmail.com>
2024-04-09 18:30:22 +0100

    update: translation task

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -10,6 +10,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """The index page
+    """
     return render_template('0-index.html')
 
 if __name__ == '__main__':
--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -20,6 +20,8 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
+    """The Index page
+    """
     return render_template('1-index.html')
 
 if __name__ == '__main__':
